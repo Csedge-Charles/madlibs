63e282c4106b6fcd83f999ee53ecb0e3a5c417b2
Charles <charles.shao@csedge.org>
2024-06-25 14:24:59 -0700

    commit_2

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,20 @@
 #Madlib by Charles
 print('Here is my first commit :) :) :) :0 -_-')
+from datetime import date
+today = date.today()
+verb_1 = str(input('Verb without tense endings: '))
+noun_1 = str(input('Noun: '))
+verb_2 = str(input('Verb without tense endings: '))
+adj_1 = str(input('Adjective: '))
+noun_2 = str(input('Noun: '))
+verb_3 = str(input('Verb with no tense endings: '))
+verb_4 = str(input('Verb with no tense endings: '))
+game = str(input(f'Game: '))
+place = str(input(f'Place: '))
+
+celebrity = str(input(f'Celebrity name: '))
+print(f'In order to {verb_1} a {noun_1}, you must {verb_2} the {adj_1} building.')
+print(f'because playing {game} in {place} {today} will {verb_1} your {noun_2}.')
+print(f'After following these steps, you can {verb_3} {celebrity} until they {verb_4}')
+print(f'Finally, you can {verb_1} a {noun_1}.')
+    
